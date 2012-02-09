01e49fca307d294e58eb2f12f494de49b13b93f4
sajmoon <simon.strom@gmail.com>
2012-02-09 17:26:41 +0100

    stupid stuff ;/

--- src/judge/Judge.java
+++ src/judge/Judge.java
@@ -1,5 +1,8 @@
 package judge;
 
+import judge.content.GlobalDictionary;
+import judge.content.Item;
+
 public class Judge {
 
 	/**
@@ -8,6 +11,22 @@ public class Judge {
 	public static void main(String[] args) {
 		System.out.println("Console Judge!");
 		System.out.println("--------------");
+		System.out.println("");
+		
+		GlobalDictionary dictionary = new GlobalDictionary();
+		dictionary.addWord("bra", 1);
+		dictionary.addWord("dålig", -1);
+		dictionary.addWord("sämst", -2);
+		dictionary.addWord("bäst", 2);
+		
+		Item i1 = new Item("jag tycker att böcker är bra");
+		Item i2 = new Item("jag tycker att böcker är dålig");
+		Item i3 = new Item("jag tycker att bra böcker är dålig");
+		
+		
+		System.out.println(">> i1: " + dictionary.getWordWeightAsString( i1 )
+				+ " i2: " + dictionary.getWordWeightAsString( i2 )
+				+ " i3: " + dictionary.getWordWeightAsString( i3 ));
 	}
 
 }
