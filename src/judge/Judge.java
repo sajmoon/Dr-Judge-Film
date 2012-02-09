cedbac289283deb3b1663d082b83ea4b139cab44
sajmoon <simon.strom@gmail.com>
2012-02-09 16:26:06 +0100

    main class! =)

---
+++ src/judge/Judge.java
@@ -0,0 +1,13 @@
+package judge;
+
+public class Judge {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Console Judge!");
+		System.out.println("--------------");
+	}
+
+}
