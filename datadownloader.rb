837185590c216c5451991b15222fe0f7cb587582
sajmoon <simon.strom@gmail.com>
2012-03-26 13:24:56 +0200

    movies and data xml

--- datadownloader.rb
+++ datadownloader.rb
@@ -1,23 +1,63 @@
 require 'twitter'
 require 'builder'
+require 'imdb'
 
 puts 'Dr Judge Film'
 puts "-------------"
 puts "v.2012-03-26"
 puts "Downloading tweets"
 
-builder = Builder::XmlMarkup.new
+def getTweets
 
-xml = ""
-Twitter.search("#LordOfTheRings", :lang => "en", :rpp => 3).map do |status|
-  "#{status.from_user}: #{status.text}"
-  xml = xml + builder.tweet { |t| t.text(status.text); t.user(status.from_user) }
+  builder = Builder::XmlMarkup.new(:indent => 2)
+
+  xml = ""
+  searchfor = [ "#LordOfTheRing", "#" ]
+
+  Twitter.search(searchfor, :lang => "en").map do |status|
+    "#{status.from_user}: #{status.text}"
+    xml = xml + builder.tweet { |t| t.text(status.text); t.user(status.from_user); t.searchterm(searchfor) }
+  end
+
+  xml
 end
 
-puts "Writing to file"
-File.open( "xmldata" , 'w') { |f| f.write(xml) }
+def writeToFile(xml, filename)
+  puts "Writing to file"
+  File.open(filename, "w") { |f| f.write(xml) }
+end
+
+def getMovieInformation(movies)
+  builder = Builder::XmlMarkup.new(:indent => 2)
+
+  moviesXml = ""
+  
+  movies.each do |m|
+    i = Imdb::Search.new(m)
+    movie = i.movies.first
+    moviesXml = moviesXml + builder.movie{ |m| m.id(movie.id); m.title(movie.title); m.url(movie.url); m.rating(movie.rating) }
+  end
+
+  moviesXml
+end
+
+def listOfMovieTitles
+  movies = ['Schindlers List', 'Inception', 'Fight Club', 'Goodfellas', 'The Matrix']
+end
+
+puts "Get movie information from IMDB"
+movies = listOfMovieTitles
+
+moviesXml = getMovieInformation(movies)
+
+writeToFile(moviesXml, "movieData.xml")
+
+
+xmlTweets = getTweets
+
+writeToFile(xmlTweets,"tweetData.xml")
+
 
 
 puts "Done, exiting"
-
 
